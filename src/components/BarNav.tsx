444c1f8f430816367d367f942cc9bea9f9014178
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-02-11 15:44:10 -0600

    stars added

--- src/components/BarNav.tsx
+++ src/components/BarNav.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/barNavstyles.module.css';
 export const BarNav = () => {
     return (
         <header className={ styles.barNav }>
-            
+                <h1> Sudoku</h1>
         </header>
     )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,16 @@
 import styles from '../styles/InitialScreen.module.css';
-
+import star from '../assets/star.png';
 interface ButtonProps {
     value: number,
 }
 
-export const Button = ({value}:ButtonProps) => 
-<button className={ styles.level }>
-    {value}
-</button>
+export const Button = ({ value }: ButtonProps) =>
+    <button className={styles.level}>
+        {value}
+        <span className={ styles.levelStatus }>
+            <img src={ star } alt="star" className={ styles.levelStatusStar }/>
+            <img src={ star } alt="star" className={ styles.levelStatusStar }/>
+            <img src={ star } alt="star" className={ styles.levelStatusStar }/>
+        </span>
+
+    </button>
